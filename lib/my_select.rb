38e0e5c4f3bee1fe8b2a3b5c02cfb9300031e5aa
Renée Cruz <reneebasscruz@gmail.com>
2019-06-17 03:32:49 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
+#select returns a new object (e.g. array) filled with only those original items where the block you gave it returned true
+
 def my_select(collection)
- # your code here!
+  i = 0
+  new_array = []
+
+    while i < collection.length 
+      
+      if 
+      new_array.push(yield(collection[i])) == true
+      end
+      i += 1
+    end
+    new_array
 end
+
