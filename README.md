2f460f9cc94d6ca556126b2b41d2143c0fd382cd
Renée Cruz <reneebasscruz@gmail.com>
2019-06-17 03:49:48 +0000

    Done.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# `my_select`
+==# `my_select`
 
 ## Objectives
 
--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,17 @@
+#select returns a new object (e.g. array) filled with only those original items where the block you gave it returned true
+
 def my_select(collection)
- # your code here!
+  i = 0
+  new_array = []
+
+    while i < collection.length 
+      
+      if 
+        yield(collection[i]) 
+        new_array.push(collection[i])
+      end
+     i += 1
+    end
+    new_array
 end
+
